870a8c0258fc37c1d93d7e631ff4d8169e110a2c
stlefebvre <lefebvre.shelby@gmail.com>
2018-06-14 01:24:31 -0400

    Created for loop, jquery to push to API

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,32 @@
 //GET methods and whatnot to determine what data gets pulled from the front end
 var friendData = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
     //Fix this to survey for loop to get paired with friend based on score
-    app.post("/api/friends", function(req, res) {
-        if (friends.length < 7) {
-            friends.push(req.body);
-            res.json(true)
+    app.post("/api/friends", function (req, res) {
+        var userInput = req.body;
+        var userResponses = userInput.scores;
+        var matchName = "";
+        var matchImage = "";
+        var totalDifference = 10000;
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                totalDifference = diff;
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
         }
+
+        friends.push(userInput);
+        res.json({
+            status: "OK",
+            matchName: matchName,
+            matchImage: matchImage
+        });
     })
-} 
+}
